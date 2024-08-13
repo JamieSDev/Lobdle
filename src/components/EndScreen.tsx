456d4ffa1bcd325ad4fd52140109e17de2ee4f35
Jamie Stuart <jamesstuart3812@gmail.com>
2024-08-12 17:30:47 -0700

    Added win text

--- src/components/EndScreen.tsx
+++ src/components/EndScreen.tsx
@@ -2,13 +2,17 @@ import { gameState } from "../App";
 import { answer } from "../App";
 
 export default function EndScreen({ restart = () => { } }) {
-    if (gameState != (1 || 2)) {
+    const text = () => {
+        return (gameState === 1) ? "Game Over" : "You Won";
+    }
+
+    if (gameState == 0) {
         return null;
     } else {
         return (
             <div className="endScreen">
-                <div className="endText">Game Over</div>
-                <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+                <div className="endText">{text()}</div>
+                <div style={{ display: "flex" }}>
                     <img className="endImage" src={answer.image} alt={answer.name}></img>
                     <button type="button" className="button" onClick={restart}>Restart</button>
                 </div>
