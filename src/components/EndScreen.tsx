88b9ce982c2d1c357ffa55b927bb4f60017c0222
Jamie Stuart <jamesstuart3812@gmail.com>
2024-08-12 17:19:16 -0700

    Added abno image and game over message

--- src/components/EndScreen.tsx
+++ src/components/EndScreen.tsx
@@ -1,15 +1,18 @@
 import { gameState } from "../App";
+import { answer } from "../App";
 
 export default function EndScreen({ restart = () => { } }) {
     if (gameState != (1 || 2)) {
         return null;
     } else {
         return (
-            <ul className="ulStyle">
-                <button type="button" className="button" onClick={restart}>
-                    Restart
-                </button>
-            </ul>
+            <div className="endScreen">
+                <div className="endText">Game Over</div>
+                <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+                    <img className="endImage" src={answer.image} alt={answer.name}></img>
+                    <button type="button" className="button" onClick={restart}>Restart</button>
+                </div>
+            </div>
         );
     }
 }
